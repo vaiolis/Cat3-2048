957548036afd18d953617e2c289be233c428ce08
orangeFlavor95 <ucsd@mingyis-MacBook.local>
2014-05-25 18:16:50 -0700

    took out the f word

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -125,7 +125,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 };
 
 HTMLActuator.prototype.message = function (won) {
-  var gameOverMessage= "Game Over"+"\n"+"fuck you";
+  var gameOverMessage= "Game Over"+"\n"+"Friend Scores";
   
   var type    = won ? "game-won" : "game-over";
   var message = won ? "You win!" : gameOverMessage;
